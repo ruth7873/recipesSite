d9add5a1599a11c45a55b91aeae44e1b4e40758a
hitmachut <hitmachut@students.local>
2024-03-12 09:48:17 +0200

    aaa

--- server/routes/category.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const categoryServer = require('../server/category')
-
-route.get('/', categoryServer.GetAllCategory)
-route.post('/', categoryServer.addCategory)
-
-module.exports = route;
